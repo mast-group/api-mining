bd1a344d7415f34569ef59222d28186a48b5cd23
Jari <jaroslav.fowkes@ed.ac.uk>
2014-09-24 11:30:44 +0100

    Add sparsity ratio calculation

--- src/main/java/itemsetmining/eval/ItemsetScaling.java
+++ src/main/java/itemsetmining/eval/ItemsetScaling.java
@@ -16,13 +16,14 @@ import java.util.HashMap;
 import java.util.Map.Entry;
 import java.util.logging.Level;
 
+import org.apache.commons.io.FileUtils;
+
 public class ItemsetScaling {
 
 	/** Main Settings */
 	private static final File dbFile = new File(
 			"/disk/data1/jfowkes/itemset.txt");
-	private static final File saveDir = new File(
-			"/afs/inf.ed.ac.uk/user/j/jfowkes/Code/Itemsets/ItemsetEval");
+	private static final File saveDir = new File("/tmp/");
 
 	/** Scaling Settings */
 	private static final int noSamples = 1;
@@ -50,8 +51,8 @@ public class ItemsetScaling {
 		// final int[] cores = new int[] { 16, 32, 48, 64 };
 		// for (final int noCores : cores)
 		// Makes sense as 10^3 * PMIN = 10
-		scalingTransactions(true, 64, new int[] { 1_000, 10_000, 100_000,
-				1_000_000, 10_000_000, 100_000_000 });
+		// scalingTransactions(true, 64, new int[] { 1_000, 10_000, 100_000 });
+		// , 1_000_000, 10_000_000, 100_000_000
 
 		// Here itemset sizes are relative
 		scalingItemsets(true, 64, new double[] { 0.05, 0.1, 0.15, 0.2 });
@@ -79,11 +80,8 @@ public class ItemsetScaling {
 		String name = InetAddress.getLocalHost().getHostName();
 		if (useSpark)
 			name = "Spark";
-		String prefix = "";
-		if (useSpark)
-			prefix += "spark_";
 		final PrintWriter out = new PrintWriter(new FileOutputStream(saveDir
-				+ "/" + prefix + name + "_scaling.txt"), true);
+				+ "/" + name + "_scaling.txt"), true);
 
 		for (int i = 0; i < trans.length; i++) {
 
@@ -131,7 +129,7 @@ public class ItemsetScaling {
 			System.out.printf("Average Time (s): %.2f%n", time[i]);
 		}
 
-		// Plot time
+		// Print time
 		System.out.println("\n========" + name + "========");
 		System.out.println("Transactions:" + Arrays.toString(trans));
 		System.out.println("Time: " + Arrays.toString(time));
@@ -150,11 +148,19 @@ public class ItemsetScaling {
 			itemsets[i] = (int) (relItemsets[i] * noTransactions);
 		final double[] time = new double[itemsets.length];
 
+		// Save to file
+		String name = InetAddress.getLocalHost().getHostName();
+		if (useSpark)
+			name = "Spark";
+		final PrintWriter out = new PrintWriter(new FileOutputStream(saveDir
+				+ "/" + name + "_scaling.txt"), true);
+
 		// Generate real itemsets
 		for (int i = 0; i < itemsets.length; i++) {
 
 			final int noSets = itemsets[i];
 			System.out.println("\n========= " + noSets + " Itemsets");
+			out.println("\n========= " + noSets + " Itemsets");
 
 			final HashMap<Itemset, Double> actualItemsets = TransactionGenerator
 					.getNoisyItemsets(noSets, MU, SIGMA, PMIN, PMAX);
@@ -169,9 +175,16 @@ public class ItemsetScaling {
 			TransactionGenerator.generateTransactionDatabase(actualItemsets,
 					noTransactions, dbFile);
 
+			// Get sparsity ratio of database
+			final double ratio = getSparsityRatio(dbFile);
+			System.out.println("Average items per transactions: " + ratio);
+			out.println("Average items per transactions: " + ratio);
+
 			for (int sample = 0; sample < noSamples; sample++) {
 				System.out.println("\n========= Sample " + (sample + 1)
 						+ " of " + noSamples);
+				out.println("\n========= Sample " + (sample + 1) + " of "
+						+ noSamples);
 
 				// Mine itemsets
 				final long startTime = System.currentTimeMillis();
@@ -186,6 +199,7 @@ public class ItemsetScaling {
 				time[i] += tim;
 
 				System.out.printf("Time (s): %.2f%n", tim);
+				out.printf("Time (s): %.2f%n", tim);
 			}
 		}
 
@@ -199,12 +213,30 @@ public class ItemsetScaling {
 			System.out.printf("Average Time (s): %.2f%n", time[i]);
 		}
 
-		String name = InetAddress.getLocalHost().getHostName();
-		if (useSpark)
-			name = "Spark";
+		// Print time
 		System.out.println("\n========" + name + "========");
 		System.out.println("Itemsets: " + Arrays.toString(itemsets));
 		System.out.println("Time: " + Arrays.toString(time));
+
+		// and save to file
+		out.println(Arrays.toString(itemsets));
+		out.println(Arrays.toString(time));
+		out.close();
+	}
+
+	/** Get sparsity ration (avg. no. items per transaction) for transaction db */
+	private static double getSparsityRatio(final File dbase) throws IOException {
+
+		final String[] lines = FileUtils.readFileToString(dbase).split("\n");
+
+		double sparsity = 0;
+		for (final String line : lines) {
+			final String[] items = line.trim().split(" ");
+			sparsity += items.length;
+		}
+		sparsity /= lines.length;
+
+		return sparsity;
 	}
 
 	private static void runSpark(final int noCores) {
