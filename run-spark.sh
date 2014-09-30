1b8c2968f3d073fbafdf6d1d9c8e4eaec0e7b899
Jari <jaroslav.fowkes@ed.ac.uk>
2014-09-30 12:57:07 +0100

    Migrate spark run script to Java 1.8

--- run-spark.sh
+++ run-spark.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0/
+export PATH=/usr/lib/jvm/java-1.8.0/bin/:$PATH
 java -cp target/driver-itemset-mining-1.1-SNAPSHOT.jar itemsetmining.main.SparkItemsetMining $*
