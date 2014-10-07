11a6d98d4a71f5fa1ce3398a89c27107cf0b1f0e
Jari <jaroslav.fowkes@ed.ac.uk>
2014-10-07 10:14:02 +0100

    Remove unused itemset simplify and combine by size algorithms

--- src/main/java/itemsetmining/main/ItemsetMiningCore.java
+++ src/main/java/itemsetmining/main/ItemsetMiningCore.java
@@ -7,14 +7,9 @@ import itemsetmining.main.InferenceAlgorithms.InferenceAlgorithm;
 import itemsetmining.transaction.TransactionDatabase;
 import itemsetmining.transaction.TransactionRDD;
 
-import java.io.Serializable;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Random;
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -24,14 +19,12 @@ import com.google.common.collect.Maps;
 import com.google.common.collect.Multiset;
 import com.google.common.collect.Ordering;
 import com.google.common.collect.Sets;
-import com.google.common.primitives.Ints;
 
 public abstract class ItemsetMiningCore {
 
 	/** Main fixed settings */
 	public static final String CANDGEN_NAME = "CombSupp";
 	private static final int OPTIMIZE_PARAMS_EVERY = 1;
-	private static final int SIMPLIFY_ITEMSETS_EVERY = 1;
 	private static final int COMBINE_ITEMSETS_EVERY = 1;
 	private static final double OPTIMIZE_TOL = 1e-5;
 
@@ -86,6 +79,15 @@ public abstract class ItemsetMiningCore {
 		// Initialize list of rejected sets
 		final Set<Itemset> rejected_sets = Sets.newHashSet();
 
+		// Define decreasing support ordering
+		final Ordering<Itemset> supportOrdering = new Ordering<Itemset>() {
+			@Override
+			public int compare(final Itemset set1, final Itemset set2) {
+				return tree.getSupportOfItemset(set2)
+						- tree.getSupportOfItemset(set1);
+			}
+		};
+
 		// Structural EM
 		boolean breakLoop = false;
 		for (int iteration = 1; iteration <= maxEMIterations; iteration++) {
@@ -94,19 +96,9 @@ public abstract class ItemsetMiningCore {
 			if (iteration % COMBINE_ITEMSETS_EVERY == 0) {
 				logger.finer("\n----- Itemset Combination at Step " + iteration
 						+ "\n");
-				transactions = combineSupportItemsetsStep(itemsets,
-						transactions, tree, rejected_sets, inferenceAlgorithm,
-						maxStructureSteps);
-				// transactions = combineItemsetsStep(itemsets,
-				// transactions, rejected_sets, inferenceAlgorithm,
-				// maxStructureSteps, new orderBySize());
-			} else if (iteration % SIMPLIFY_ITEMSETS_EVERY == 0) {
-				logger.finer("\n----- Itemset Simplification at Step "
-						+ iteration + "\n"); // TODO use dedicated maxSteps
-												// parameter?
-				transactions = simplifyItemsetsStep(itemsets, transactions,
+				transactions = combineItemsetsStep(itemsets, transactions,
 						tree, rejected_sets, inferenceAlgorithm,
-						maxStructureSteps);
+						maxStructureSteps, supportOrdering);
 			} else {
 				logger.finer("\n+++++ Tree Structural Optimization at Step "
 						+ iteration + "\n");
@@ -273,83 +265,6 @@ public abstract class ItemsetMiningCore {
 		logger.warning("\n\n Structure iteration limit exceeded. No better candidate found.\n");
 		transactions.setIterationLimitExceeded();
 		return transactions;
-	}
-
-	/** Generate candidate itemsets from power set */
-	private static TransactionDatabase simplifyItemsetsStep(
-			final HashMap<Itemset, Double> itemsets,
-			final TransactionDatabase transactions, final ItemsetTree tree,
-			final Set<Itemset> rejected_sets,
-			final InferenceAlgorithm inferenceAlgorithm, final int maxSteps) {
-
-		// Try and find better itemset to add
-		logger.finer(" Structural candidate itemsets: ");
-
-		// Sort itemsets from largest to smallest // TODO skip sorting?
-		final List<Itemset> sortedItemsets = Lists.newArrayList(itemsets
-				.keySet());
-		Collections.sort(sortedItemsets,
-				new orderBySize().reverse()
-						.compound((Ordering.usingToString())));
-
-		// Suggest powersets for all itemsets
-		int iteration = 0;
-		for (final Itemset set : sortedItemsets) {
-
-			// Subsample if |items| > 30 TODO better heuristic?
-			Set<Integer> setItems;
-			if (set.size() > 30)
-				setItems = subSample(set, 30);
-			else
-				setItems = set.getItems();
-
-			final Set<Set<Integer>> powerset = Sets.powerSet(setItems);
-			for (final Set<Integer> subset : powerset) {
-
-				// Evaluate candidate itemset
-				final Itemset candidate = new Itemset(subset);
-				if (!rejected_sets.contains(candidate)) {
-					final TransactionDatabase betterCost = evaluateCandidate(
-							itemsets, transactions, tree, inferenceAlgorithm,
-							candidate);
-					if (betterCost != null) // Better itemset found
-						return betterCost;
-					else
-						rejected_sets.add(candidate); // otherwise add to
-														// rejected
-				}
-
-				iteration++;
-				if (iteration > maxSteps) { // Iteration limit exceeded
-					logger.warning("\n Simplify iteration limit exceeded.\n");
-					return transactions; // No better itemset found
-				}
-
-			}
-		}
-
-		// No better itemset found
-		logger.finer("\n No better candidate found.\n");
-		return transactions;
-	}
-
-	/** Generate candidates by combining existing itemsets with highest support */
-	private static TransactionDatabase combineSupportItemsetsStep(
-			final HashMap<Itemset, Double> itemsets,
-			final TransactionDatabase transactions, final ItemsetTree tree,
-			final Set<Itemset> rejected_sets,
-			final InferenceAlgorithm inferenceAlgorithm, final int maxSteps) {
-
-		final Ordering<Itemset> supportOrdering = new Ordering<Itemset>() {
-			@Override
-			public int compare(final Itemset set1, final Itemset set2) {
-				return tree.getSupportOfItemset(set2)
-						- tree.getSupportOfItemset(set1);
-			}
-		};
-
-		return combineItemsetsStep(itemsets, transactions, tree, rejected_sets,
-				inferenceAlgorithm, maxSteps, supportOrdering);
 	}
 
 	/**
@@ -620,43 +535,6 @@ public abstract class ItemsetMiningCore {
 		}
 
 		return interestingnessMap;
-	}
-
-	private static class orderBySize extends Ordering<Itemset> implements
-			Serializable {
-		private static final long serialVersionUID = -5940108461179194842L;
-
-		@Override
-		public int compare(final Itemset set1, final Itemset set2) {
-			return Ints.compare(set1.size(), set2.size());
-		}
-	};
-
-	/**
-	 * Algorithm to randomly subsample a set
-	 *
-	 * @param items
-	 *            Collection of items
-	 * @param m
-	 *            number of items to subsample
-	 * @return subsampled set
-	 *
-	 * @see http://eyalsch.wordpress.com/2010/04/01/random-sample/
-	 */
-	public static <T> Set<T> subSample(final Collection<T> items, int m) {
-		final Random rnd = new Random();
-		final HashSet<T> res = new HashSet<T>(m);
-		int visited = 0;
-		final Iterator<T> it = items.iterator();
-		while (m > 0) {
-			final T item = it.next();
-			if (rnd.nextDouble() < ((double) m) / (items.size() - visited)) {
-				res.add(item);
-				m--;
-			}
-			visited++;
-		}
-		return res;
 	}
 
 }
