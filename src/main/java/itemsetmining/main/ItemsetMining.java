c3f251e3b88415b7116e374e71c817050180d2fb
Jari <jaroslav.fowkes@ed.ac.uk>
2015-01-05 11:25:40 +0000

    Add CLI to local (non spark) implementation

--- src/main/java/itemsetmining/main/ItemsetMining.java
+++ src/main/java/itemsetmining/main/ItemsetMining.java
@@ -5,6 +5,7 @@ import itemsetmining.itemset.ItemsetTree;
 import itemsetmining.itemset.Rule;
 import itemsetmining.main.InferenceAlgorithms.InferGreedy;
 import itemsetmining.main.InferenceAlgorithms.InferenceAlgorithm;
+import itemsetmining.main.SparkItemsetMining.LogLevelConverter;
 import itemsetmining.transaction.Transaction;
 import itemsetmining.transaction.TransactionList;
 import itemsetmining.util.Logging;
@@ -21,6 +22,9 @@ import java.util.logging.Level;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.LineIterator;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import com.google.common.base.Charsets;
 import com.google.common.base.Functions;
 import com.google.common.collect.HashMultiset;
@@ -33,35 +37,67 @@ import com.google.common.io.Files;
 
 public class ItemsetMining extends ItemsetMiningCore {
 
+	/** Main function parameters */
+	public static class Parameters {
+
+		@Parameter(names = { "-f", "--file" }, description = "Dataset filename")
+		private final File dataset = new File(
+				"/afs/inf.ed.ac.uk/user/j/jfowkes/Code/Itemsets/Datasets/Succintly/mammals.dat");
+
+		@Parameter(names = { "-s", "--maxSteps" }, description = "Max structure steps")
+		int maxStructureSteps = 100_000_000;
+
+		@Parameter(names = { "-i", "--iterations" }, description = "Max iterations")
+		int maxEMIterations = 1_000;
+
+		@Parameter(names = { "-l", "--log-level" }, description = "Log level", converter = LogLevelConverter.class)
+		Level logLevel = Level.FINE;
+
+		@Parameter(names = { "-r", "--runtime" }, description = "Max Runtime (min)")
+		long maxRunTime = 12 * 60; // 12hrs
+
+		@Parameter(names = { "-t", "--timestamp" }, description = "Timestamp Logfile", arity = 1)
+		boolean timestampLog = true;
+	}
+
 	public static void main(final String[] args) throws IOException {
 
-		// Main function parameters
-		final File dataset = new File(
-				"/afs/inf.ed.ac.uk/user/j/jfowkes/Code/Itemsets/Datasets/Succintly/mammals.dat");
+		// Main fixed parameters
 		final boolean associationRules = false;
 		final InferenceAlgorithm inferenceAlg = new InferGreedy();
 
 		// Runtime parameters
-		final int maxStructureSteps = 100_000_000;
-		final int maxEMIterations = 1_000;
-		MAX_RUNTIME = 12 * 60 * 60 * 1_000; // 12hrs
-		LOG_LEVEL = Level.FINE;
-		final File logFile = Logging.getLogFileName("IIM", true, LOG_DIR,
-				dataset);
+		final Parameters params = new Parameters();
+		final JCommander jc = new JCommander(params);
 
-		// Mine interesting itemsets
-		final Map<Itemset, Double> itemsets = mineItemsets(dataset,
-				inferenceAlg, maxStructureSteps, maxEMIterations, logFile);
+		try {
+			jc.parse(args);
 
-		// Generate Association rules from the interesting itemsets
-		if (associationRules) {
-			final List<Rule> rules = generateAssociationRules(itemsets);
-			System.out
-					.println("\n============= ASSOCIATION RULES =============");
-			for (final Rule rule : rules) {
-				System.out.println(rule.toString());
+			// Set loglevel, runtime, timestamp and log file
+			LOG_LEVEL = params.logLevel;
+			MAX_RUNTIME = params.maxRunTime * 60 * 1_000;
+			final File logFile = Logging.getLogFileName("IIM",
+					params.timestampLog, LOG_DIR, params.dataset);
+
+			// Mine interesting itemsets
+			final Map<Itemset, Double> itemsets = mineItemsets(params.dataset,
+					inferenceAlg, params.maxStructureSteps,
+					params.maxEMIterations, logFile);
+
+			// Generate Association rules from the interesting itemsets
+			if (associationRules) {
+				final List<Rule> rules = generateAssociationRules(itemsets);
+				System.out
+						.println("\n============= ASSOCIATION RULES =============");
+				for (final Rule rule : rules) {
+					System.out.println(rule.toString());
+				}
+				System.out.println("\n");
 			}
-			System.out.println("\n");
+
+		} catch (final ParameterException e) {
+			System.out.println(e.getMessage());
+			jc.usage();
 		}
 
 	}
