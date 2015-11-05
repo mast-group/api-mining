7e986ad5c3739c4fbba06100971c7c49238a5720
Jari <jaroslav.fowkes@ed.ac.uk>
2015-11-05 11:43:04 +0000

    Better precision-recall graphs

--- scripts/PrecisionRecall.py
+++ scripts/PrecisionRecall.py
@@ -1,12 +1,15 @@
 import matplotlib.pyplot as plt
+from matplotlib import rcParams
 from numpy import array, arange, nan
+import os
 
 def main():
     
-    baseFolder = '/afs/inf.ed.ac.uk/user/j/jfowkes/Code/Sequences/Datasets/API/examples/'
-    algs = ['ISM-Int','ISM-Prob','MAPO','UP-Miner']
+    baseFolder = '/afs/inf.ed.ac.uk/user/j/jfowkes/Code/Sequences/Datasets/API/examples/all/'
+    algs = ['ISM-Prob','MAPO','UP-Miner']
     ubound = 'Dataset'
     markers = ['o','D','s','v','^','*','x','+','d','.']
+    rcParams['axes.color_cycle'] = ['b','g','r','m']
     
     for alg in algs:
     
@@ -29,26 +32,35 @@ def main():
         fl.close()    
         
         symbol = markers[algs.index(alg)]
-        plotfigpr(seqPrecision,seqRecall,'Sequence','Average Sequence Recall','Interpolated Average Sequence Precision',symbol,1) 
+        plotfigpr(seqPrecision,seqRecall,'','Average Sequence Recall','Interpolated Average Sequence Precision',symbol,1) 
         plotfigpr(mprecision,mrecall,'Method','Average Method Recall','Interpolated Average Method Precision',symbol,2)
-        plotfig(arange(0,501,10),redundancy,'Redundancy','top N','Average Inter-sequence Distance',symbol,3)
-        plotfig(arange(0,501,10),spuriousness,'Spuriousness','top N','Average no. containing subsequences',symbol,4)
+        plotfig(arange(10,501,10),redundancy,'Redundancy','top N','Average Inter-sequence Distance',symbol,3)
+        plotfig(arange(10,501,10),spuriousness,'','top N','Average no. containing subsequences',symbol,4)
         
     fu = open(baseFolder+ubound+'.pr','r')
     vals = fu.readline().split(',')
+    print 'Average Dataset Sequence Precision: ' + vals[0]
+    print 'Averaga Dataset Sequence Recall: ' + vals[1]
+    print 'Average Min. Edit Distance Examples/Test calls to Dataset: ' + vals[4]
     mRecallMax = float(vals[3])
-    fu.close()    
-    plotdashed(mRecallMax, 0.2,2)
+    fu.close()   
+    plotdashed(mRecallMax,2)
 
+    algs[algs.index('ISM-Prob')] = 'PAM'
     plt.figure(1)
     plt.legend(algs,loc='lower right')
-    plt.figure(3)
-    plt.legend(algs,loc='upper right')
-    plt.figure(4)
-    plt.legend(algs,loc='upper right')
+    plt.savefig(baseFolder+'SequencePrecisionRecall.png',dpi=150)
+    savepdf('SequencePrecisionRecall',baseFolder)
     plt.figure(2)
     plt.legend(algs,loc='lower right')
-    
+    plt.savefig(baseFolder+'MethodPrecisionRecall.png',dpi=150)
+    plt.figure(3)
+    plt.legend(algs,loc='lower right')
+    plt.savefig(baseFolder+'InterSeqDistance.png',dpi=150)
+    plt.figure(4)
+    plt.legend(algs,loc='upper left')
+    plt.savefig(baseFolder+'ContainingSubseqs.png',dpi=150)
+    savepdf('ContainingSubseqs',baseFolder)
     plt.show()
 
 # Interpolate precision
@@ -72,15 +84,15 @@ def plotfigpr(precision,recall,title,xlabel,ylabel,symbol,figno):
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.title(title)
-    plt.xlim([0,1])
+    plt.xlim([0,0.8])
+    plt.ylim([0,0.8])
     plt.grid(True)
 
 # Plot dashed horizontal
-def plotdashed(maxRecall,ulim,figno):
+def plotdashed(maxRecall,figno):
 
     plt.figure(figno) 
     plt.plot(array([maxRecall,maxRecall]),array([0,1]),'k--',linewidth=2) 
-    plt.xlim([0,ulim])
 
 # Plot figure
 def plotfig(x,y,title,xlabel,ylabel,symbol,figno):
@@ -92,5 +104,10 @@ def plotfig(x,y,title,xlabel,ylabel,symbol,figno):
     plt.ylabel(ylabel)
     plt.title(title)
     plt.grid(True)
+
+def savepdf(name,folder):
+    plt.savefig(folder+name+'.eps',format='eps',dpi=1200)
+    os.system('epstopdf '+folder+name+'.eps')
+    os.system('rm '+folder+name+'.eps')
 
 main()
